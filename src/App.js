af9bcc8fb86717bf9f9186b79bcb511d1e21bffb
Leo Dimacuha <leo.dimacuha@gmail.com>
2019-01-15 16:46:22 +1000

    add google analytics tracking

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import M from "materialize-css/dist/js/materialize.min.js";
+import ReactGA from 'react-ga';
 
 import './App.css';
 
 import AppContainer from './components/AppContainer';
 
 class App extends Component {
+  initializeReactGA() {
+    ReactGA.initialize('UA-132445894-1');
+    ReactGA.pageview('/');
+  }
+
   componentDidMount() {
     M.AutoInit();
+    this.initializeReactGA();
   }
+
   render() {
     return (
       <AppContainer />
